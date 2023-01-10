802f82c430b5cc7f77cf6e43ecb10d90c0fd468c
Riteshgupta2906 <80892162+Riteshgupta2906@users.noreply.github.com>
2023-01-11 04:52:16 +0530

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { Routes, Route, Router } from "react-router-dom";
-import { Layout } from "antd";
-import Desktop from "./Desktop";
-function App() {
-  return <Desktop />;
-}
-
-export default App;
